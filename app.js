2b51488df5e5779a0f14af241400dc18996cfc27
jmunozc023 <jmunozc023@ulacit.ed.cr>
2024-12-08 21:48:58 -0600

    Update app.js
    Agregado el CRUD para base de datos usuarios

--- app.js
+++ app.js
@@ -97,3 +97,85 @@ app.delete('/api/destinos/:id', (req, res) => { // Ruta para eliminar un destino
         res.status(400).json({ Error: 'Id de destino no valido' });
     }
 })
+app.get('/api/usuarios', (req, res) => { // Ruta para obtener todos los usuarios
+    const page = req.query.page || 0; 
+    const usuariosPerPage = 20; // Número de usuarios por página
+    let usuarios = []; // Arreglo para almacenar los usuarios
+    db.collection('usuarios') // Consulta los usuarios
+    .find()
+    .sort({ id: 1 }) // Ordena los usuarios por id
+    .skip(usuariosPerPage * page) // Salta los usuarios de las páginas anteriores
+    .limit(usuariosPerPage)
+    .forEach(usuario => usuarios.push(usuario)) // Almacena los usuarios en el arreglo
+    .then(() => {
+        res.status(200).json(usuarios); // Envia los usuarios en formato JSON
+    })
+    .catch(() => {
+        res.status(500).json({ error: 'Error al consultar los usuarios' });
+    });
+})
+app.get('/api/usuarios/:id', (req, res) => { // Ruta para obtener un usuario por id
+    const usuarioId = parseInt(req.params.id); // Obtiene el id del usuario
+    if(!isNaN(usuarioId)){ // Verifica que el id sea un número
+        db.collection('usuarios')
+        .findOne({ id: usuarioId }) // Consulta el usuario por id
+        .then(usuario => {
+            if(usuario){
+                res.status(200).json(usuario); // Envia el destino en formato JSON
+            } else {
+                res.status(404).json({ msg: 'Usuario no encontrado' });
+            }
+        })
+        .catch(() => {
+            res.status(500).json({ msg: 'Error al consultar el usuario' });
+        })
+    } else {
+        res.status(400).json({ Error: 'Id de usuario no valido' });
+    }
+})
+app.post('/api/usuarios', (req, res) => { // Ruta para insertar un usuario
+    const usuario = req.body; // Obtiene el destino del body
+    db.collection('usuarios')
+    .insertOne(usuario) // Inserta el usuario
+    .then((result) => {
+        res.status(201).json({ result });
+    })
+    .catch(() => {
+        res.status(500).json({ msg: 'Error al insertar el usuario' });
+    })
+})
+app.patch('/api/usuarios/:id', (req, res) => { // Ruta para actualizar un usuario por id
+    let update = req.body; // Obtiene los datos a actualizar del body
+    const usuarioId = parseInt(req.params.id); // Obtiene el id del usuario
+    if(!isNaN(usuarioId)){ // Verifica que el id sea un número
+        db.collection('usuarios')
+        .updateOne({ id: usuarioId }, { $set: update }) // Actualiza el usuario por id
+        .then((result) => {
+            if(result.matchedCount > 0){
+                res.status(200).json({ result });
+            } else {
+                res.status(404).json({ msg: 'Usuario no encontrado' });
+            }
+        })
+        .catch(() => {
+            res.status(500).json({ msg: 'Error al actualizar el usuario' });
+        })
+    } else {
+        res.status(400).json({ Error: 'Id de usuario no valido' });
+    }
+})
+app.delete('/api/usuarios/:id', (req, res) => { // Ruta para eliminar un usuario por id
+    const usuarioId = parseInt(req.params.id); // Obtiene el id del usuario
+    if(!isNaN(usuarioId)){ // Verifica que el id sea un número
+        db.collection('usuarios')
+        .deleteOne({ id: usuarioId }) // Elimina el usuario por id
+        .then((result) => {
+            res.status(204).json({ result });
+        })
+        .catch(() => {
+            res.status(500).json({ msg: 'Error al eliminar el usuario' });
+        })
+    } else {
+        res.status(400).json({ Error: 'Id de usuario no valido' });
+    }
+})
