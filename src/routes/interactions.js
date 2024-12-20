ebad1554d81305ce23be6d5d58432b270b49f2a9
jmunozc023 <jmunozc023@ulacit.ed.cr>
2024-12-20 16:56:31 -0600

    Cambios esteticos
    cambios esteticos

--- src/routes/interactions.js
+++ src/routes/interactions.js
@@ -8,5 +8,11 @@ router.get('/gestion-itinerario', (req, res) => {
     res.render('gestion-itinerario');
 });
 
+router.get('/mapa-interactivo', (req, res) => {
+    res.render('mapa-interactivo');
+});
+router.get('/pagos', (req, res) => {
+    res.render('pagos');
+});
 
 module.exports = router;
