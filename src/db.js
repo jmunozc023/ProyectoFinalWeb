e1b1ee36433055bd3d8efade5719e1bdd6bf9570
Jose Muñoz Carvajal <jmunozc023@ulacit.ed.cr>
2025-03-02 18:48:44 -0600

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://josepmunoz85:Ulacit2412$@cluster0.uptj2.mongodb.net/ProyFin')
+mongoose.connect('')
     .then(db => console.log('DB is connected'))
     .catch(err => console.error(err));
